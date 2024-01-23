34bd741c07c7f817fa390e76d258d71f783ef376
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-23 14:44:01 +0100

    delete css

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
+//index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
