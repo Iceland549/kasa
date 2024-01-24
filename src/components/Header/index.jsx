d2d15d9d380eab8bcfaa95e3c845acfb94dbc612
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-24 13:22:48 +0100

    add header function

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.jpg'; 
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Logo" />
+      <nav>
+        <ul>
+          <li><Link to="/">Accueil</Link></li>
+          <li><Link to="/About">A propos</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
