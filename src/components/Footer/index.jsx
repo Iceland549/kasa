4e6cb1cd6a46cb530c401a41fb24b5a12587c264
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-24 13:22:35 +0100

    add footer function

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/logo.jpg'; 
+
+function Footer() {
+  return (
+    <footer>
+      <img src={logo} alt="Logo" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
