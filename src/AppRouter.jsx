535907d21c379153961738252adf71b464e71ae0
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-24 13:23:17 +0100

    add layout import

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,7 @@
 // AppRouter.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import LocationPage1 from './pages/LocPage1';
@@ -14,17 +15,19 @@ import Error404 from './pages/Error404';
 const AppRouter = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About/>} />
-        <Route path="/LocPage1" element={<LocationPage1 />} />
-        <Route path="/LocPage2" element={<LocationPage2 />} />
-        <Route path="/LocPage3" element={<LocationPage3 />} />
-        <Route path="/LocPage4" element={<LocationPage4 />} />
-        <Route path="/LocPage5" element={<LocationPage5 />} />
-        <Route path="/LocPage6" element={<LocationPage6 />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="/About" element={<About/>} />
+                <Route path="/LocPage1" element={<LocationPage1 />} />
+                <Route path="/LocPage2" element={<LocationPage2 />} />
+                <Route path="/LocPage3" element={<LocationPage3 />} />
+                <Route path="/LocPage4" element={<LocationPage4 />} />
+                <Route path="/LocPage5" element={<LocationPage5 />} />
+                <Route path="/LocPage6" element={<LocationPage6 />} />
+            </Route>    
+            <Route path="*" element={<Error404 />} />
+        </Routes>
     </Router>
   );
 };
