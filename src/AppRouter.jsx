7ec8b297b67c92c62c38cb100c15e3de2131fa24
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-24 09:29:52 +0100

    modufy routes

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,6 @@
 // AppRouter.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import LocationPage1 from './pages/LocPage1';
@@ -15,15 +15,15 @@ const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact component={Home} />
-        <Route path="/About" exact component={About} />
-        <Route path="/LocPage1" exact component={LocationPage1} />
-        <Route path="/LocPage2" exact component={LocationPage2} />
-        <Route path="/LocPage3" exact component={LocationPage3} />
-        <Route path="/LocPage4" exact component={LocationPage4} />
-        <Route path="/LocPage5" exact component={LocationPage5} />
-        <Route path="/LocPage6" exact component={LocationPage6} />
-        <Route component={Error404} />
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/LocPage1" element={<LocationPage1 />} />
+        <Route path="/LocPage2" element={<LocationPage2 />} />
+        <Route path="/LocPage3" element={<LocationPage3 />} />
+        <Route path="/LocPage4" element={<LocationPage4 />} />
+        <Route path="/LocPage5" element={<LocationPage5 />} />
+        <Route path="/LocPage6" element={<LocationPage6 />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
   );
