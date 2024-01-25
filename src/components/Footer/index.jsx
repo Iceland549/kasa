b885f992d45921e222a20a4ae1432bc54516cef2
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-25 11:20:27 +0100

    import footer style and add className footer_logo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../../assets/logo.jpg'; 
+import './_footer.scss'
 
 function Footer() {
   return (
     <footer>
-      <img src={logo} alt="Logo" />
+      <img src={logo} className='footer_logo' alt="Logo" />
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
