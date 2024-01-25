5aa51078b479dffe29dc9cf128c24e4505767311
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-25 09:38:33 +0100

    add Banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner1 from '../../assets/banner_accueil.jpg'; 
+
+function Banner() {
+  return (
+    <div>
+      <img src={banner1} alt="Paysage côtier rocheux" />
+    </div>
+  );
+};
+
+export default Banner;
