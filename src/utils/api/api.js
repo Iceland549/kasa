4cd60c8ebe0abf9ed82ebc599206ab91f8588c58
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-30 09:33:10 +0100

    add api.js

---
+++ src/utils/api/api.js
@@ -0,0 +1,6 @@
+export async function getLogements() {
+    const response = await fetch("http://localhost:3000/kasa.json");
+    const logements = await response.json();
+  
+    return logements;
+}
