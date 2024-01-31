1b41c05d2e93327bf52472b25aae234e070adaa9
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-31 08:58:24 +0100

    add text to 404 page

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 function Error404() {
   return (
     <div>
-      <h1>Error404</h1>
+      <h1>404</h1>
+      <p>Oups ! La page que vous demandez n'exite pas.</p>
+      <Link to={`/`}>
+        <p>Retourner sur la page d'acceuil</p>
+      </Link>
+
     </div>
   );
 };
