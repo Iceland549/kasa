81e76f0432075da211ae324ebe49a4a80e6fb0c2
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-31 08:58:05 +0100

    add function About

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,21 @@
+// About
 import React from 'react';
+import Banner from '../../components/Banner';
+import DropdownMenu from '../../components/DropdownMenu'; 
 
 function About() {
+  const text1 = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes";
+  const text2 = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme";
+  const text3 = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme";
+  const text4 = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.";
+
   return (
     <div>
-      <h1>A propos</h1>
+      <Banner />
+      <DropdownMenu title="Fiabilité" >{text1}</DropdownMenu> 
+      <DropdownMenu title="Respect" >{text2}</DropdownMenu> 
+      <DropdownMenu title="Service" >{text3}</DropdownMenu>
+      <DropdownMenu title="Sécurité" >{text4}</DropdownMenu>
     </div>
   );
 };
