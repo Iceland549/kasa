78672e673ef113942c3fd36f913b5cfa31042f9b
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-31 08:58:59 +0100

    add Card to Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import Banner from '../../components/Banner'
+import React, { useEffect, useState } from 'react';
+import Banner from '../../components/Banner';
+import Card from '../../components/Card';
+import { getLogements } from '../../utils/api/api';
+
 
 function Home() {
+  const [logements, setLogements] = useState([])
+
+  useEffect(()=>{
+    const fetchLogements = async () => {
+      const datas = await getLogements()
+      setLogements(datas)
+    }
+    fetchLogements()
+  }, [])
+
   return (
     <div>
         <Banner />
+        <div className="card_container">
+          {logements.map(logement => (
+            <Card key={logement.id} id={logement.id} title={logement.title} cover={logement.cover} />
+          ))}
+        </div>
     </div>
   );
 };
