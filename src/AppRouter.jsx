e8c06b63ec7ffa69c9f0afb309283ca5324688c6
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-31 08:55:57 +0100

    delete LocPage 2 to 6

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -4,12 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
-import LocationPage1 from './pages/LocPage1';
-import LocationPage2 from './pages/LocPage2';
-import LocationPage3 from './pages/LocPage3';
-import LocationPage4 from './pages/LocPage4';
-import LocationPage5 from './pages/LocPage5';
-import LocationPage6 from './pages/LocPage6';
+import LocationPage from './pages/LocPage';
 import Error404 from './pages/Error404';
 
 function AppRouter() {
@@ -18,12 +13,7 @@ function AppRouter() {
         <Routes>
             <Route path="/" element={<Layout><Home /></Layout>} />
             <Route path="/About" element={<Layout><About/></Layout>} />
-            <Route path="/LocPage1" element={<Layout><LocationPage1 /></Layout>} />
-            <Route path="/LocPage2" element={<Layout><LocationPage2 /></Layout>} />
-            <Route path="/LocPage3" element={<Layout><LocationPage3 /></Layout>} />
-            <Route path="/LocPage4" element={<Layout><LocationPage4 /></Layout>} />
-            <Route path="/LocPage5" element={<Layout><LocationPage5 /></Layout>} />
-            <Route path="/LocPage6" element={<Layout><LocationPage6 /></Layout>} />
+            <Route path="/LocPage/:id" element={<Layout><LocationPage /></Layout>} />
             <Route path="*" element={<Error404 />} />
         </Routes>
     </Router>
