1553e015d0066ab1f005763b404892774117db1a
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-02-01 14:44:49 +0100

    add function LocPage

--- src/pages/LocPage/index.jsx
+++ src/pages/LocPage/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getLogements } from '../../utils/api/api';
+import { useParams } from 'react-router-dom';
+import DropdownMenu from '../../components/DropdownMenu';
+import Tags from '../../components/Tags';
+import Carousel from '../../components/Carousel'; 
 
 function LocPage() {
+  const [logement, setLogement] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchLogement = async () => {
+      const logements = await getLogements();
+      const logement = logements.find(logement => logement.id === id);
+      setLogement(logement);
+    };
+    fetchLogement();
+  }, [id]); 
+
+  if (!logement) {
+    return <div>Loading...</div>
+  }
+
+  const { title, pictures, host, rating, location, description, equipments, tags } = logement;
+
   return (
     <div>
-      <h1>LocPage</h1>
+      <Carousel images={pictures} />
+      <h1>{title}</h1>
+      <p>{location}</p>
+      <p>{host.name}</p>
+      <p>Rating: {rating}</p>
+      <DropdownMenu description={description} equipments={equipments} id={id} />
+      <Tags tags={tags} id={id} />
     </div>
   );
 };
