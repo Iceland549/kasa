3293723859101cdd7749c1759dd752e800199595
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-02-01 14:41:28 +0100

    add props to banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import banner1 from '../../assets/banner_accueil.jpg'; 
 import './_banner.scss'
 
 
-function Banner() {
+function Banner({image, children}) {
   return (
     <div className="banner">
-      <img src={banner1} className="banner_img1" alt="Paysage côtier rocheux" />
-      <div className="banner_text">Chez vous, partout et ailleurs</div>
+      <img src={image} className="banner_img1" alt="Paysage côtier rocheux" />
+      {children}
     </div>
   );
 };
