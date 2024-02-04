e5eba9334ab6cab2e2f92591df0cd5bcbca062f5
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-02-04 16:53:01 +0100

    add <br/> to error function

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -8,7 +8,7 @@ function Error() {
   return (
     <div className='error-container'>
       <h1>404</h1>
-      <p>Oups ! La page que vous demandez n'exite pas.</p>
+      <p>Oups ! La page que <br/> vous demandez n'exite pas.</p>
       <Link to={`/`}>
         <p className='error-link'> Retourner sur la page d'acceuil</p>
       </Link>
