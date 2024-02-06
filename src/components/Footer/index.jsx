2e945879606784fb49eee97951e99c48a00d8e5d
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-02-06 19:06:04 +0100

    add <br/> to footer 'p'

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer>
       <img src={logo} className='footer_logo' alt="Logo" />
-      <p>© 2020 Kasa. All rights reserved</p>
+      <p>© 2020 Kasa. All <br/> rights reserved</p>
     </footer>
   );
 };
