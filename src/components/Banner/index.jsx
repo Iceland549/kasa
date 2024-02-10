f586d2507437139db30106a594defa77f10e73e6
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-02-10 13:25:15 +0100

    add alt to props of Banner function

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './_banner.scss'
 
 
-function Banner({image, children}) {
+function Banner({image, alt, children}) {
   return (
     <div className="banner">
-      <img src={image} className="banner_img1" alt="Paysage côtier rocheux" />
+      <img src={image} className="banner_img1" alt={alt} />
       {children}
     </div>
   );
