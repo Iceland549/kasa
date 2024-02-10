6022f11a4c72b6ae2c5115f2343a73a1281fe927
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-02-10 13:24:40 +0100

    change div to article in Card function

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 function Card({id, title, cover}) {
   return (
     <Link to={`/LocPage/${id}`}>
-      <div className='card'>
+      <article className='card'>
         <img src={cover} alt={title} />
         <h2>{title}</h2>
-      </div>
+      </article>
     </Link>
 
   );
